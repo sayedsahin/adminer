2ecb80a407b3dbf6a2172d142e1f5bba1d991087
Sayed Sahin <sahin.sayed1@gmail.com>
2025-06-18 13:27:19 +0600

    add sqlite

--- sqlite.php
+++ sqlite.php
@@ -1,10 +1,10 @@
 <?php
 function adminer_object() {
-	include_once "../plugins/login-password-less.php";
+	include_once "plugins/login-password-less.php";
 	return new Adminer\Plugins(array(
 		// TODO: inline the result of password_hash() so that the password is not visible in source codes
 		new AdminerLoginPasswordLess(password_hash("root", PASSWORD_DEFAULT)),
 	));
 }
 
-include "./index.php";
+include "./adminer.php";
