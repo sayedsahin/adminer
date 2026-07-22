3e339db6a9c649ed317d4af2906081d1f28e1897
sayedsahin <sahinsayed@gmail.com>
2026-07-22 21:18:21 +0600

    Revert "index file replaced by sqlite code"
    This reverts commit 53f9ffd41c39fbde913e59416b29d83298275daa.

--- index.php
+++ index.php
@@ -1,11 +1,2 @@
 <?php
-//  Adminer With Login with password less for sqlite purpose
-function adminer_object() {
-	include_once "plugins/login-password-less.php";
-	return new Adminer\Plugins(array(
-		// TODO: inline the result of password_hash() so that the password is not visible in source codes
-		new AdminerLoginPasswordLess(password_hash("root", PASSWORD_DEFAULT)),
-	));
-}
-
-include "./adminer.php";
+require __DIR__ . '/adminer.php';
