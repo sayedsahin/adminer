53f9ffd41c39fbde913e59416b29d83298275daa
sayedsahin <sahinsayed@gmail.com>
2026-07-22 20:20:24 +0600

    index file replaced by sqlite code

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
-require __DIR__ . '/adminer.php';
+//  Adminer With Login with password less for sqlite purpose
+function adminer_object() {
+	include_once "plugins/login-password-less.php";
+	return new Adminer\Plugins(array(
+		// TODO: inline the result of password_hash() so that the password is not visible in source codes
+		new AdminerLoginPasswordLess(password_hash("root", PASSWORD_DEFAULT)),
+	));
+}
+
+include "./adminer.php";
